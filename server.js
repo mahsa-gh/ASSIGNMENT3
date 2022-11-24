10276cc7d86faf70fff60a9e58682c3bc6067209
mahsa <mghasemi5@myseneca.ca>
2022-11-24 11:59:59 +0400

    Assignment 6

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /*********************************************************************************
-*  WEB322 – Assignment 05
+*  WEB322 – Assignment 06
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
 *  assignment has been copied manually or electronically from any other source (including web sites) or 
 *  distributed to other students.
 * 8
-*  Name: Mahsa Ghasemi  Student ID: 152449195  Date: November 18th
+*  Name: Mahsa Ghasemi  Student ID: 152449195  Date: 1th December
 *
 *  Online (Cyclic) Link: https://smoggy-dog-mittens.cyclic.app/
 *
-********************************************************************************/ 
+********************************************************************************/
 
 
 
@@ -21,6 +21,8 @@ const app = express();
 const multer = require('multer')
 const fs = require('fs')
 const handleBars = require('express-handlebars')
+const clientSessions = require('client-sessions')
+
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -36,9 +38,24 @@ const upload = multer({ storage: storage })
 
 
 const data = require(path.join(__dirname, 'blog-service.js'));
+const dataServiceAuth = require(path.join(__dirname, 'data-service-auth.js'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
+
+app.use(clientSessions({
+  cookieName: "session",
+  secret: "MahsaGhasemi",
+  duration: 10 * 60 * 1000,
+  activeDuration: 1000 * 60
+}))
+
+app.use(function (req, res, next) {
+  res.locals.session = req.session;
+  next();
+});
+
+
 
 app.engine('.hbs', handleBars.engine({
   extname: '.hbs', helpers: {
@@ -77,7 +94,7 @@ app.get('/about', (req, res, next) => {
   res.render('about');
 });
 
-app.get('/students', (req, res, next) => {
+app.get('/students', ensureLogin, (req, res, next) => {
   if (req.query.status) {
     return data.getStudentsByStatus(req.query.status)
       .then(data => {
@@ -127,7 +144,7 @@ app.get('/students', (req, res, next) => {
     });
 });
 
-app.get('/student/:sid', (req, res) => {
+app.get('/student/:sid', ensureLogin, (req, res) => {
   // initialize an empty object to store the values
   let viewData = {};
 
@@ -167,33 +184,20 @@ app.get('/student/:sid', (req, res) => {
 });
 
 
-app.post('/students/add', (req, res) => {
+app.post('/students/add', ensureLogin, (req, res) => {
   data.addStudent(req.body).then(
     res.redirect('/students')
   )
     .catch(err => console.log(err))
 })
 
-app.post("/student/update", (req, res) => {
+app.post("/student/update", ensureLogin, (req, res) => {
   data.updateStudent(req.body).then(() => {
     res.redirect("/students");
   }).catch(err => console.log(err))
 });
 
-
-app.get('/intlstudents', (req, res, next) => {
-  data
-    .getInternationalStudents()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      console.log('Error retrieving managers: ' + err);
-      res.json({ message: err });
-    });
-});
-
-app.get('/programs', (req, res, next) => {
+app.get('/programs', ensureLogin, (req, res, next) => {
   data
     .getPrograms()
     .then((data) => {
@@ -209,7 +213,7 @@ app.get('/programs', (req, res, next) => {
     });
 });
 
-app.get('/students/add', (req, res) => {
+app.get('/students/add', ensureLogin, (req, res) => {
   data.getPrograms().
     then(data => {
       res.render("addStudent", { programs: data });
@@ -218,14 +222,14 @@ app.get('/students/add', (req, res) => {
       res.render("addStudent", { programs: [] });
     })
 })
-app.get('/images/add', (req, res) => {
+app.get('/images/add', ensureLogin, (req, res) => {
   res.render('addImage');
 })
-app.post('/images/add', upload.single('imageFile'), (req, res) => {
+app.post('/images/add', ensureLogin, upload.single('imageFile'), (req, res) => {
   res.redirect('/images')
 })
 
-app.get('/images', (req, res) => {
+app.get('/images', ensureLogin, (req, res) => {
   fs.readdir("./public/images/uploaded", function (err, data) {
     if (err) return console.log(err)
     console.log(data)
@@ -235,21 +239,21 @@ app.get('/images', (req, res) => {
     })
   })
 })
-app.get('/programs/add', (req, res) => {
+app.get('/programs/add', ensureLogin, (req, res) => {
   res.render("addProgram")
 })
 
-app.post('/programs/add', (req, res) => {
+app.post('/programs/add', ensureLogin, (req, res) => {
   data.addProgram(req.body).then(() => { res.redirect('/programs') })
 })
 
-app.post("/programs/add", (req, res) => {
+app.post("/programs/add", ensureLogin, (req, res) => {
   data.updateProgram(req.body).then(() => {
     res.redirect("/programs");
   }).catch(err => console.log(err))
 });
 
-app.get('/program/:programCode', (req, res) => {
+app.get('/program/:programCode', ensureLogin, (req, res) => {
   data.getProgramByCode(req.params.programCode)
     .then((data) => {
       if (data.length > 0) {
@@ -261,17 +265,59 @@ app.get('/program/:programCode', (req, res) => {
     .catch(err => console.log(err))
 })
 
-app.get('/programs/delete/:programCode', (req, res) => {
+app.get('/programs/delete/:programCode', ensureLogin, (req, res) => {
   data.deleteProgramByCode(req.params.programCode).then(() => {
     res.redirect('/programs')
   }).catch(err => { res.status(500).send("Unable to Remove Program / Program not found)"); })
 })
-app.get('/students/delete/:studentID', (req, res) => {
+app.get('/students/delete/:studentID', ensureLogin, (req, res) => {
   data.deleteStudentById(req.params.studentID).then(() => {
     res.redirect('/students')
   }).catch(err => { res.status(500).send("Unable to Remove Student / Student not found"); })
 })
 
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', (req, res) => {
+  req.body.userAgent = req.get('User-Agent');
+
+  dataServiceAuth.checkUser(req.body).then((user) => {
+
+    req.session.user = {
+      userName: user.userName,
+      email: user.email,
+      loginHistory: user.loginHistory
+    }
+    console.log(req.session.user.loginHistory)
+    res.redirect('/students')
+  }).catch(err => {
+    res.render('login', { errorMessage: err, userName: req.body.userName })
+  })
+
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', (req, res) => {
+  dataServiceAuth.registerUser(req.body).then(() => {
+    res.render('register', { successMessage: "User created" })
+  }).catch(err => {
+    res.render('register', { errorMessage: err, userName: req.body.userName })
+  })
+})
+
+app.get('/userHistory', ensureLogin, (req, res) => {
+  res.render('userHistory')
+})
+
+app.get('/logout', (req, res) => {
+  req.session.reset();
+  res.redirect("/")
+})
 
 app.use((req, res, next) => {
   res.status(404).send('Page Not Found');
@@ -279,6 +325,7 @@ app.use((req, res, next) => {
 
 data
   .initialize()
+  .then(dataServiceAuth.initialize)
   .then(() => {
     app.listen(HTTP_PORT);
     console.log('Express http server listening on ' + HTTP_PORT);
@@ -286,3 +333,12 @@ data
   .catch((err) => {
     console.log('Error starting server: ' + err + ' aborting startup');
   });
+
+
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+}
